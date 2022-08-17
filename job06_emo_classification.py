572b4f7ebed68dc5365dcff8b6b0632e30585c95
dduoyong <dayoung.biz@gmail.com>
2022-08-17 12:47:52 +0900

    K-meneans_emo_classification_03

--- job06_emo_classification.py
+++ job06_emo_classification.py
@@ -10,22 +10,24 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import normalize
 from sklearn.cluster import KMeans
 
-data = pd.read_csv('./Melon/04_audio_features_data/Dance_lyric_and_audio.csv')
+data = pd.read_csv('./Melon/04_audio_features_data/Pop_lyric_and_audio.csv')
 print(data.head())
 
+#----loudeness, teompo 값 MinMaxScaler----
 scaler = MinMaxScaler()
 data.loudness = scaler.fit_transform(data.loudness.values.reshape(-1,1))
 data.tempo = scaler.fit_transform(data.tempo.values.reshape(-1,1))
-print(data.head())
+# print(data.head())
 
 
+#----'mode' column 값 삭제, Audio_Feature 값 데이터프레임화----
 scaled_data = pd.DataFrame(data, columns= data.columns[4:10])
 scaled_data.drop(['mode'], axis=1, inplace=True)
 scaled_data.info()
 print(scaled_data.head())
 
 
-#----heatmap----
+#----Aduio Features Heatmap----
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
 sns.heatmap(scaled_data.corr(),cmap=cmap)
 plt.show()
@@ -34,7 +36,7 @@ plt.show()
 plt.figure(figsize=(5, 4))
 plt.title("Dendrograms")
 shc.dendrogram(shc.linkage(scaled_data, method = 'ward'))
-plt.axhline(y=4, color='r', linestyle='--')
+plt.axhline(y=6, color='r', linestyle='--')
 plt.show()
 
 #----K-Means clustering----
@@ -58,18 +60,28 @@ fig.show()
 plt.show()
 
 
-label_origin = ['Euphoric', 'Cheerful','Happy',  'Angry', 'Depressed', 'Sad']
+#----clustering 데이터 6개 감정 분류----
+label_origin = ['Euphoric', 'Cheerful','Happy', 'Angry', 'Depressed', 'Sad']
 print(scaled_data['label'].value_counts())
+
 label_name_df = scaled_data.groupby('label')[['valence']].mean()
 label_name_df.reset_index(inplace=True, drop=False)
 print(label_name_df)
+
 label_name_df.sort_values('valence', inplace=True, ascending=False)
 print(label_name_df)
+
 label_trans = [None] * 6
 for i in range(len(label_name_df)):
     label_trans[label_name_df.iloc[i, 0]] = label_origin[i]
 print('lt', label_trans)
+
 scaled_data['emo'] = 0
 for i in range(len(scaled_data)):
     scaled_data.loc[i, 'emo'] = label_trans[scaled_data.loc[i, 'label']]
 print(scaled_data.head())
+
+#----감정 라벨링 column 값 csv 파일 저장----
+data['emo'] = scaled_data['emo']
+data = data[['artist','title','lyric','emo','track_id','danceability','energy','loudness','acousticness','valence','tempo']]
+data.to_csv('./Pop_6emo.csv', index=False)
